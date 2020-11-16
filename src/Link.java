0cafa933b12209bef5cde1cc45596019f6bcc950
chisacam <dladsds123@gmail.com>
2020-11-17 04:08:41 +0900

    add fileio for logging

--- src/Link.java
+++ src/Link.java
@@ -5,6 +5,8 @@ public class Link implements Runnable {
     long startTime; // 링크 객체 생성시간
     AtomicBoolean isBusy = new AtomicBoolean(false); // 링크 사용가능 여부
     AtomicBoolean isEnd = new AtomicBoolean(false); // 통신 종료 여부
+    String sender;
+    String receiver;
     Node[] node = new Node[4];
     public Link() {
         for(int i = 0; i < 4; i++)
@@ -13,18 +15,23 @@ public class Link implements Runnable {
         t = new Thread(this, "Link");
         t.start();
     }
-    public boolean requestUseLink(String nodeName) { // 노드의 링크 사용 가능여부를 반환
-        System.out.println(nodeName + " request to use Link");
+    public synchronized boolean requestUseLink(String nodeName, String target) { // 노드의 링크 사용 가능여부를 반환
+        System.out.println(System.currentTimeMillis() - startTime + " " + nodeName + " Data Send Request To " + target);
         if(isBusy.get()) {
             // 링크가 사용 불가능한 경우 false
-            System.out.println(nodeName + " request reject because Link is busy");
+            System.out.println(System.currentTimeMillis() - startTime + " " + "Reject: " + nodeName + " Data Send Request To " + target);
             return false;
         } else {
             // 링크가 사용 가능한 경우 true, isBusy 또한 사용가능으로 변경
-            System.out.println(nodeName + " request is accept");
+            System.out.println(System.currentTimeMillis() - startTime + " " + "Accept: " + nodeName + " Data Send Request To " + target);
             isBusy.set(true);
+            sender = nodeName;
+            receiver = target;
             return true;
         }
+    }
+    public void alertFinish(String nodeName, String target) {
+        System.out.println(System.currentTimeMillis() - startTime + " " + nodeName + " Data Send Finished To " + target);
     }
     public void run() {
         while(true){
--- src/Node.java
+++ src/Node.java
@@ -1,12 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Node implements Runnable {
     Thread t; // 쓰레드 객체
     Link link; // 링크 객체
     int retryNum; // 전송 요청 실패횟수
-
+    List<String> list = new ArrayList<>(Arrays.asList("Node1", "Node2", "Node3", "Node0"));
+    String target;
     // 노드 초기화, 생성자에서 스레드 생성 및 시작
     public Node(String name, Link link) {
+        list.remove(name);
         this.link = link;
         t = new Thread(this, name);
         t.start();
@@ -22,35 +29,62 @@ public class Node implements Runnable {
     }
     public void run() {
         Random rand = new Random();
+        target = list.get(rand.nextInt(3));
+
         try {
             Thread.sleep(rand.nextInt(30)); //스레드 별로 임의의 시간에 요청해야 하므로 run 초기에도 랜덤 sleep 시킴
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        while(System.currentTimeMillis() - link.startTime <= 60000) { // 동작조건은 시작시간으로부터 1분간
-            if(!link.requestUseLink(t.getName())){ // link 객체에 사용여부 질의
-                // 사용불가 상태일경우 backoff 반환 시간만큼 대기
-                try {
-                    System.out.println(t.getName() + " Data Send Request Reject from Link");
-                    int waitTime = BackoffTimer(retryNum++);
-                    System.out.println(t.getName() + " Exponential Back-off Time: " + waitTime + " msec");
-                    Thread.sleep(waitTime);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else {
-                // 사용가능한 상태일 경우 5초대기(데이터전송시간)후 링크 상태를 사용가능으로 변경
-                try {
-                    System.out.println(t.getName() + " Data Send Request Accept from Link");
-                    Thread.sleep(5);
-                    System.out.println(t.getName() + " Data Send Request Complete");
-                    link.isBusy.set(false);
-                } catch (Exception e) {
-                    e.printStackTrace();
+        try {
+            FileWriter fw = new FileWriter(t.getName() + ".txt", false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            while(System.currentTimeMillis() - link.startTime <= 60000) { // 동작조건은 시작시간으로부터 1분간
+                if(!link.requestUseLink(t.getName(), target)){ // link 객체에 사용여부 질의
+                    // 사용불가 상태일경우 backoff 반환 시간만큼 대기
+                    try {
+                        bw.write(System.currentTimeMillis() - link.startTime + " " + "Data Send Request To " + target);
+                        bw.newLine();
+                        bw.write(System.currentTimeMillis() - link.startTime + " " + t.getName() + " Data Send Request Reject from Link");
+                        bw.newLine();
+                        int waitTime = BackoffTimer(retryNum++);
+                        bw.write(System.currentTimeMillis() - link.startTime + " " + t.getName() + " Exponential Back-off Time: " + waitTime + " msec");
+                        bw.newLine();
+                        Thread.sleep(waitTime);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    // 사용가능한 상태일 경우 5초대기(데이터전송시간)후 링크 상태를 사용가능으로 변경
+                    try {
+                        if(t.getName().equals(link.sender)) {
+                            bw.write(System.currentTimeMillis() - link.startTime + " " + "Data Send Request To " + target);
+                            bw.newLine();
+                            bw.write(System.currentTimeMillis() - link.startTime + " " + t.getName() + " Data Send Request Accept from Link");
+                            bw.newLine();
+                            Thread.sleep(5);
+                            bw.write(System.currentTimeMillis() - link.startTime + " " + t.getName() + " Data Send Finished To " + target);
+                            bw.newLine();
+                        } else {
+                            bw.write(System.currentTimeMillis() - link.startTime + " " + "Data Receive Start from " + link.sender);
+                            bw.newLine();
+                            Thread.sleep(5);
+                            bw.write(System.currentTimeMillis() - link.startTime + " " + "Data Receive Finished from " + link.sender);
+                            bw.newLine();
+                        }
+                        link.isBusy.set(false);
+                        link.alertFinish(t.getName(), target);
+                        target = list.get(rand.nextInt(3));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-            System.out.println(System.currentTimeMillis() - link.startTime + " " + t.getName());
+            bw.close();
+        } catch (Exception e) {
+            System.out.println(e);
         }
+
         link.isEnd.set(true);
         System.out.println(t.getName() + " Finished");
     }
